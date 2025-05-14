3f8dc56c19c8badedddef5c988fc8877c3fe92e5
Jeremy <jjwilke@users.noreply.github.com>
2025-05-14 10:14:43 -0700

    install numactl and nsys in devcontainer (#51)

---
+++ docker/scripts/tools/install.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+apt-get update
+
+apt-get install numactl
+
+for arg in "$@"; do
+ case "$arg" in
+    nsys) nsys=1 ;;
+    *) echo "Unknown component: $arg" ;;
+  esac
+done
+
+if [ ! -z $nsys ]; then
+
+pushd opt
+curl -fsSL https://developer.download.nvidia.com/devtools/repos/ubuntu2204/amd64/nvidia.pub | apt-key add -
+echo "deb https://developer.download.nvidia.com/devtools/repos/ubuntu2204/amd64/ /" >> /etc/apt/sources.list.d/nsys.list
+
+wget https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/2024_1/nsight-systems-2024.1.1_2024.1.1.59-1_amd64.deb
+apt-get update
+dpkg --configure -a
+apt-get -y -f install
+apt-get -y install nsight-systems-2024.1.1 --reinstall
+
+mkdir -p /root/.config/NVIDIA\ Corporation
+echo "CuptiUsePerThreadBuffer=false" > /root/.config/NVIDIA\ Corporation/nsys-config.ini
+
+fi
