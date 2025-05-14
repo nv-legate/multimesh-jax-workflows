a573587e488e2c8a2850a15b2fba57caa649ef89
Murali Andoorveedu <37849411+andoorve@users.noreply.github.com>
2025-05-14 13:28:45 -0700

    Update bootstrap-devcontainer.sh to use the correct folder to check git submodule existence (#55)
    * Update bootstrap-devcontainer.sh
    
    Signed-off-by: Murali Andoorveedu <37849411+andoorve@users.noreply.github.com>
    
    * Update bootstrap-devcontainer.sh
    
    Signed-off-by: Murali Andoorveedu <37849411+andoorve@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Murali Andoorveedu <37849411+andoorve@users.noreply.github.com>

--- .devcontainer/bootstrap-devcontainer.sh
+++ .devcontainer/bootstrap-devcontainer.sh
@@ -8,7 +8,7 @@ topdir=$1
 # convert the submodules into full directories
 pushd $topdir
 for folder in "xla" "multimesh-jax" "jax" "realm" "zuku"; do
-  if [ ! -d "docker/workspace/$folder" ]; then
+  if [ ! -e "docker/workspace/$folder/.git" ]; then
     git submodule update --init --recursive -- docker/workspace/$folder
   fi
 done
