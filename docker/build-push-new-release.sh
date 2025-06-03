022130b2e189b2eceeed70712effc641451cfa2a
jjwilke <jwilke@nvidia.com>
2025-06-03 07:28:28 -0700

    release-v0.1

--- docker/build-push-new-release.sh
+++ docker/build-push-new-release.sh
@@ -3,4 +3,4 @@
 #                         All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-./build.py --stage distribute --tag $1 --repo ghcr.io/nv-legate --upload --name multimesh-jax
+./build.py --stage final_image --tag $1 --repo ghcr.io/nv-legate --upload --name multimesh-jax
